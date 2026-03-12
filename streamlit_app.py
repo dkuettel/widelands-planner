1d51b9f8349831cb4fcbe67e2fdee9dab3c3af13
Daniel Kuettel <dkuettel@gmail.com>
2026-03-12 19:36:15 +0100

    better indicators

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import streamlit as st
+from streamlit.delta_generator import DeltaGenerator
 
 buildings = {
     "forester",
@@ -28,30 +29,27 @@ names = st.data_editor(["main"], num_rows="dynamic")
 
 for name, tab in zip(names, st.tabs(names), strict=True):
     with tab:
-        st.title("state")
-
-        with st.container(horizontal=True):
-            counts = {
-                b: st.number_input(label=b, min_value=0, value=0, key=f"{name}/{b}")
-                for b in sorted(buildings)
-            }
+        counts: dict[str, int] = dict()
+        actions: dict[str, DeltaGenerator] = dict()
+        for b in sorted(buildings):
+            col1, col2 = st.columns([0.3, 0.7], border=True)
+            with col1:
+                counts[b] = st.number_input(
+                    label=b, min_value=0, value=0, key=f"{name}/{b}"
+                )
+            with col2:
+                actions[b] = st.empty()
 
         needs: dict[str, float] = dict()
         for b, c in counts.items():
             for nb, nc in requires.get(b, dict()).items():
                 needs[nb] = needs.get(nb, 0.0) + c * nc
+
         needs = {
             b: (c - counts.get(b, 0)) for b, c in needs.items() if c > counts.get(b, 0)
         }
 
-        if needs:
-            st.title("actions")
-            st.write(
-                {
-                    b: {
-                        "add": round(n, 1),
-                        "for": required.get(b, set()),
-                    }
-                    for b, n in needs.items()
-                }
+        for b, n in needs.items():
+            actions[b].write(
+                f"add {round(n, 1)} for: {','.join(required.get(b, set()))}"
             )
