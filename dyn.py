e9f3fc100392818060521fa2c42b09ce22f1cc76
Daniel Kuettel <dkuettel@gmail.com>
2026-03-16 13:45:51 +0100

    import export

--- dyn.py
+++ dyn.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import math
 from dataclasses import dataclass
 from functools import partial
 from typing import Literal
@@ -75,7 +76,7 @@ def get_block_building(id: int, name: BuildingName) -> int:
 
 
 def get_direct_needs(block: int) -> dict[BuildingName, float]:
-    needs: dict[BuildingName, float] = dict()
+    needs: dict[BuildingName, float] = {name: 0.0 for name in buildings}
     for building in buildings:
         for other, count in requires.get(building, {}).items():
             needs[other] = (
@@ -111,54 +112,85 @@ def st_block(bid: int):
     ):
         with st.container(horizontal=True, vertical_alignment="bottom"):
             st.selectbox("type", sorted(block_infos), key=bkey("type"))
-            st.text_input(f"name for block id {bid}", key=bkey("name"))
+            st.text_input("name", key=bkey("name"))
             if st.button("delete", key=bkey("delete")):
                 delete_block(bid)
                 st.rerun()
 
-        with st.container(horizontal=True):
-            needs: dict[BuildingName, float] = get_direct_needs(bid)
-            for name in sorted(buildings):
-                if (
-                    name in get_block_info(bid).buildings
-                    or bbcount(name) > 0
-                    or needs.get(name, 0) > 0
-                ):
-                    with st.container(border=True, width=200):
-                        if name in get_block_info(bid).exports:
-                            st.number_input(
-                                f"{name} - exported",
-                                min_value=0,
-                                key=bbkey(name),
-                            )
-                        elif name in get_block_info(bid).imports:
-                            st.number_input(
-                                f"{name} - imported",
-                                min_value=0,
-                                key=bbkey(name),
-                                # TODO hmm this could be non-zero from a previous type? and then you cant change it
-                                disabled=True,
-                            )
-                        else:
-                            st.number_input(
-                                name,
-                                min_value=0,
-                                key=bbkey(name),
-                            )
-                        match needs.get(name, None):
-                            case None | 0 | 0.0:
-                                if (
-                                    name in get_block_info(bid).exports
-                                    or bbcount(name) == 0
-                                ):
-                                    st.write("no need")
-                                else:
-                                    st.write("no need :warning:")
-                            case float(c) | int(c):
-                                if bbcount(name) < c:
-                                    st.write(f"needs {c:.1f} :warning:")
-                                else:
-                                    st.write(f"needs {c:.1f}")
+        bi = get_block_info(bid)
+        needs: dict[BuildingName, float] = get_direct_needs(bid)
+
+        with st.container():
+            if bi.exports:
+                st.write("**exports**")
+                with st.container(horizontal=True):
+                    for name in sorted(bi.exports):
+                        with st.container(border=True, width=200):
+                            add = needs[name] - bbcount(name)
+                            if math.ceil(add) > 0:
+                                label = f"{name} :warning: add {math.ceil(add)}"
+                            elif math.ceil(-add) > 1:
+                                label = f"{name} :warning: rm {math.ceil(-add) - 1}"
+                            else:
+                                label = name
+                            st.number_input(label, min_value=0, key=bbkey(name))
+                            add = needs[name] - bbcount(name)
+                            if bbcount(name) > 0:
+                                usage = round(100 * needs[name] / bbcount(name))
+                                st.text(
+                                    "\n".join(
+                                        [
+                                            f"{100 - usage}% export = {bbcount(name) - needs[name]:.1f}/{bbcount(name)}",
+                                            f"{usage}% local = {needs[name]:.1f}/{bbcount(name)}",
+                                        ]
+                                    )
+                                )
+                            else:
+                                st.text("no export\nno import")
+
+            if bi.buildings - bi.exports - bi.imports:
+                st.write("**local**")
+                with st.container(horizontal=True):
+                    # TODO rename bi.buildings to bi.local and dont overlap?
+                    for name in sorted(bi.buildings - bi.exports - bi.imports):
+                        with st.container(border=True, width=200):
+                            add = needs[name] - bbcount(name)
+                            if math.ceil(add) > 0:
+                                label = f"{name} :warning: add {math.ceil(add)}"
+                            elif math.ceil(-add) > 1:
+                                label = f"{name} :warning: rm {math.ceil(-add) - 1}"
+                            else:
+                                label = name
+                            st.number_input(label, min_value=0, key=bbkey(name))
+                            if bbcount(name) > 0:
+                                usage = round(100 * needs[name] / bbcount(name))
+                                st.text(
+                                    f"{usage}% usage = {needs[name]:.1f}/{bbcount(name)}"
+                                )
+                            else:
+                                st.text("no production")
+
+            misplaced = (
+                {
+                    name
+                    for name in buildings
+                    if bbcount(name) > 0 or (needs[name] > 0 and name not in bi.imports)
+                }
+                - bi.buildings
+                - bi.exports
+            )
+            if misplaced:
+                st.write("**misplaced** :warning:")
+                with st.container(horizontal=True):
+                    for name in sorted(misplaced):
+                        with st.container(border=True, width=200):
+                            st.number_input(name, min_value=0, key=bbkey(name))
+
+            if bi.imports:
+                st.write("**imports**")
+                with st.container(horizontal=True):
+                    for name in sorted(bi.imports):
+                        st.write(needs[name], name)
 
 
 def main():
